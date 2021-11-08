5f76308b2e49b097b34b830ab5d91aa7edc985fb
KohamaKaito <e195746@ie.u-ryukyu.ac.jp>
2021-11-09 03:55:56 +0900

    test

--- public/index.js
+++ public/index.js
@@ -1,29 +1,29 @@
-function signout() {
-    firebase.auth().signOut()
-      .then(() => {
-        console.log('Signed out')
-      })
-  }
+function signout2() {
+  firebase.auth().signOut()
+    .then(() => {
+      console.log('Signed out')
+    })
+}
 
-  firebase.auth().onAuthStateChanged((user) => {
-    if (user) {
-      document.getElementById('sign-in-status').innerText = 'Signed in'
-      document.getElementById('sign-out').style.display = 'block'
-    } else {
-      document.getElementById('sign-in-status').innerText = 'Signed out'
-      document.getElementById('sign-out').style.display = 'none'
-    }
-  })
-  
-  var ui = new firebaseui.auth.AuthUI(firebase.auth());
-  console.log(typeof ui)
-  ui.start('#firebaseui-auth-container', {
-    signInFlow: 'popup',
-    signInSuccessUrl: './otamesi.html',
-    signInOptions: [
-      {
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    document.getElementById('sign-in-status').innerText = 'Signed in'
+    document.getElementById('sign-out').style.display = 'block'
+  } else {
+    document.getElementById('sign-in-status').innerText = 'Signed out'
+    document.getElementById('sign-out').style.display = 'none'
+  }
+})
+
+var ui2 = new firebaseui.auth.AuthUI(firebase.auth());
+console.log(typeof ui2)
+ui2.start('#firebaseui-auth-container', {
+  signInFlow: 'popup',
+  signInSuccessUrl: './otamesi.html',
+  signInOptions: [
+    {
       provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
       requireDisplayName: false
-      },
-    ]
-  });
+    },
+  ]
+});
