04bc7509cd21bb6addcfa71c93ed5f0887941227
ひじ方　敦也 <e195749@ie.u-ryukyu.ac.jp>
2021-11-09 01:28:56 +0900

    second commit

---
+++ public/index.js
@@ -0,0 +1,29 @@
+function signout() {
+    firebase.auth().signOut()
+      .then(() => {
+        console.log('Signed out')
+      })
+  }
+
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      document.getElementById('sign-in-status').innerText = 'Signed in'
+      document.getElementById('sign-out').style.display = 'block'
+    } else {
+      document.getElementById('sign-in-status').innerText = 'Signed out'
+      document.getElementById('sign-out').style.display = 'none'
+    }
+  })
+  
+  var ui = new firebaseui.auth.AuthUI(firebase.auth());
+  console.log(typeof ui)
+  ui.start('#firebaseui-auth-container', {
+    signInFlow: 'popup',
+    signInSuccessUrl: './otamesi.html',
+    signInOptions: [
+      {
+      provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      requireDisplayName: false
+      },
+    ]
+  });
---
+++ public/js/config.js
@@ -0,0 +1,11 @@
+// コンソールの内容をそのままコピペ
+var config = {
+    apiKey: "AIzaSyCsmz_vqciQDjXHh9l8ekTqiERG4AowB5g",
+    authDomain: "ed-game-project.firebaseapp.com",
+    projectId: "ed-game-project",
+    storageBucket: "ed-game-project.appspot.com",
+    messagingSenderId: "112968060654",
+    appId: "1:112968060654:web:7ca93cb3f8ee05fb876dff",
+    measurementId: "G-P52X757W15"
+  };
+  firebase.initializeApp(config);
