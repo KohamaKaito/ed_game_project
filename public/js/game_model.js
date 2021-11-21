cd9f745583eac34a7fc8da2218ff4d8a4f3393ab
ひじ方　敦也 <e195749@ie.u-ryukyu.ac.jp>
2021-11-21 15:34:08 +0900

    処理判定の実装

--- public/js/game_model.js
+++ public/js/game_model.js
@@ -33,8 +33,129 @@ function switchPlayer(){
 
 
 // 勝者を判定する関数
-function judge(row, column, playerNumber){
+function judge(row, column){
+    let coinCount = 1;
+    // 上確認
+    for(let i=0; i < 3; i++){
+        if(row-i ==0){
+            break;
+        }
+        if(board[row-1-i][column] == activePlayer){
+            coinCount += 1; 
+        }
+        else{
+            break;
+        }
+    }
+    // 下確認
+    for(let i=0; i < 3; i++){
+        if(row+i == 5){
+            break;
+        }
+        if(board[row+1+i][column] == activePlayer){
+            coinCount += 1; 
+        }
+        else{
+            break;
+        }
+    }
+    // 上または下の結果
+    if(coinCount == 4){
+        return activePlayer;
+    }
+    else{
+        coinCount = 1;
+    }
 
+    // 右確認
+    for(let i=0; i < 3; i++){
+        if(board[row][column+1+i] == activePlayer){
+            coinCount += 1; 
+        }
+        else{
+            break;
+        }
+    }
+    // 左確認
+    for(let i=0; i < 3; i++){
+        if(board[row][column-1-i] == activePlayer){
+            coinCount += 1; 
+        }
+        else{
+            break;
+        }
+    }
+    // 右または左の結果
+    if(coinCount == 4){
+        return activePlayer;
+    }
+    else{
+        coinCount = 1;
+    }
+
+    // 右上確認
+    for(let i=0; i < 3; i++){
+        if(row-i == 0 || column+i == 6){
+            break;
+        }
+        if(board[row-1-i][column+1+i] == activePlayer){
+            coinCount += 1; 
+        }
+        else{
+            break;
+        }
+    }
+    // 左下確認
+    for(let i=0; i < 3; i++){
+        if(row+i == 5 || column-i == 0){
+            break;
+        }
+        if(board[row+1+i][column-1-i] == activePlayer){
+            coinCount += 1; 
+        }
+        else{
+            break;
+        }
+    }
+    // 右上または左下の結果
+    if(coinCount == 4){
+        return activePlayer;
+    }
+    else{
+        coinCount = 1;
+    }
+
+    // 左上確認
+    for(let i=0; i < 3; i++){
+        if(row-i == 0 || column-i == 0){
+            break;
+        }
+        if(board[row-1-i][column-1-i] == activePlayer){
+            coinCount += 1; 
+        }
+        else{
+            break;
+        }
+    }
+    // 右下確認
+    for(let i=0; i < 3; i++){
+        if(row+i == 5 || column+i == 6){
+            break;
+        }
+        if(board[row+1+i][column+1+i] == activePlayer){
+            coinCount += 1; 
+        }
+        else{
+            break;
+        }
+    }
+    // 左上または右下の結果
+    if(coinCount == 4){
+        return activePlayer;
+    }
+    else{
+        return 0;
+    }
 }
 
 
@@ -44,6 +165,7 @@ function insertCoin1(){
         if(board[i][0] == 0){
             board[i][0] = activePlayer;
             drawCoin(i,0,activePlayer);
+            console.log(judge(i,0));
             switchPlayer();
             break;
         }
@@ -54,6 +176,7 @@ function insertCoin2(){
         if(board[i][1] == 0){
             board[i][1] = activePlayer;
             drawCoin(i,1,activePlayer);
+            console.log(judge(i,1));
             switchPlayer();
             break;
         }
@@ -64,6 +187,7 @@ function insertCoin3(){
         if(board[i][2] == 0){
             board[i][2] = activePlayer;
             drawCoin(i,2,activePlayer);
+            console.log(judge(i,2));
             switchPlayer();
             break;
         }
@@ -74,6 +198,7 @@ function insertCoin4(){
         if(board[i][3] == 0){
             board[i][3] = activePlayer;
             drawCoin(i,3,activePlayer);
+            console.log(judge(i,3));
             switchPlayer();
             break;
         }
@@ -84,6 +209,7 @@ function insertCoin5(){
         if(board[i][4] == 0){
             board[i][4] = activePlayer;
             drawCoin(i,4,activePlayer);
+            console.log(judge(i,4));
             switchPlayer();
             break;
         }
@@ -94,6 +220,7 @@ function insertCoin6(){
         if(board[i][5] == 0){
             board[i][5] = activePlayer;
             drawCoin(i,5,activePlayer);
+            console.log(judge(i,5));
             switchPlayer();
             break;
         }
@@ -104,6 +231,7 @@ function insertCoin7(){
         if(board[i][6] == 0){
             board[i][6] = activePlayer;
             drawCoin(i,6,activePlayer);
+            console.log(judge(i,6));
             switchPlayer();
             break;
         }
